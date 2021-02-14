6fd9b651972b1cd29a679065206bd9055617b86a
Kanrin_Cyou <zhaoguanlun@gmail.com>
2021-02-14 20:08:12 +0900

    finished the pop up card

--- src/pages/mainpage/portfolioDetail/portfolioDetail.jsx
+++ src/pages/mainpage/portfolioDetail/portfolioDetail.jsx
@@ -12,6 +12,7 @@ class PortfolioDetail extends Component {
       }
     
     houseData = this.props.detail()
+  
 
     render(){
       return (
@@ -19,34 +20,57 @@ class PortfolioDetail extends Component {
 
           <div className="close" onClick={()=>{this.props.routeChange()}}/>
           <div className="housetitle">
-              <h2>{this.houseData.structureType}</h2>
+              <div className="pf-button"><p>{this.houseData.structureType}</p></div>
               <h2>{this.houseData.name}</h2>
           </div>
-          <div className="portfoiliocontent">
-            <div className="picSlider">
-              <AwesomeSlider>
-                <div data-src={this.houseData.img}/>
-                <div data-src={this.houseData.img2}/>
-                <div data-src={this.houseData.img3}/>
-              </AwesomeSlider>
-            </div>
 
-            <div className="housedetail">
-              <ul>
-                <li>所在地:{this.houseData.address}</li>
-                <li>最寄駅:{this.houseData.traffic}</li>
-                <li>間取:{this.houseData.style}</li>
-                <li>面積{this.houseData.area}</li>
-                <li>築年数:{this.houseData.finishedDate}</li>
-                <li>賃料{this.houseData.rental}</li>
-                <li>管理費:{this.houseData.maintenance}</li>
-                <li>敷金/礼金:{this.houseData.otherfee}</li>
-              </ul>
-            </div>
+          <div className="portfoiliocontent">
+              <div className="carousel">
+                <AwesomeSlider bullets={false}>
+                  <div className="detailimg"><img src={this.houseData.img} alt="img1"/></div>
+                  <div className="detailimg"><img src={this.houseData.img2} alt="img2"/></div>
+                  <div className="detailimg"><img src={this.houseData.img3} alt="img3"/></div>
+                </AwesomeSlider>
+              </div>
+                <div className="housedetail">
+                  <ul>
+                    <ul>
+                      <li className='houselist listtitle'><p>所在地:</p></li>
+                      <li className='houselist'><p>{this.houseData.address}</p></li>
+                    </ul>
+                    <ul>
+                      <li className='houselist listtitle'><p>最寄駅:</p></li>
+                      <li className='houselist'><p>{this.houseData.traffic}</p></li>
+                    </ul>
+                    <ul>
+                      <li className='houselist listtitle'><p>間取:</p></li>
+                      <li className='houselist'><p>{this.houseData.style}</p></li>
+                    </ul>
+                    <ul>
+                      <li className='houselist listtitle'><p>面積:</p></li>
+                      <li className='houselist'><p>{this.houseData.area}</p></li>
+                    </ul>
+                    <ul>
+                      <li className='houselist listtitle'><p>築年数:</p></li>
+                      <li className='houselist'><p>{this.houseData.finishedDate}</p></li>
+                    </ul>
+                    <ul>
+                      <li className='houselist listtitle'><p>賃料:</p></li>
+                      <li className='houselist'><p>{this.houseData.rental}</p></li>
+                    </ul>
+                    <ul>
+                      <li className='houselist listtitle'><p>管理費:</p></li>
+                      <li className='houselist'><p>{this.houseData.maintenance}</p></li>
+                    </ul>
+                    <ul>
+                      <li className='houselist listtitle'><p>敷金/礼金:</p></li>
+                      <li className='houselist'><p>{this.houseData.otherfee}</p></li>
+                    </ul>
+                  </ul>
+                </div>
           </div>
         </div>
-    );
-  }
+    );}
   }
   
   export default PortfolioDetail;
